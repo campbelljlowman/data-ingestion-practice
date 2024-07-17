8fd71a5a13a1b8fd70918ffe65caa962cd089109
Campbell Lowman <campbelllowman@gmail.com>
2024-07-17 16:40:24 -0400

    Add fake location generator and avro data schema

--- Kafka/kafkaConsumer.ts
+++ Kafka/kafkaConsumer.ts
@@ -1,4 +1,10 @@
 import { Kafka } from 'kafkajs'
+import avro from 'avsc';
+
+const locationAvroSchemaFile = Bun.file('./locationAvroSchema.json')
+const locationAvroSchemaJSON = await locationAvroSchemaFile.json()
+const locationAvroType = avro.Type.forSchema(locationAvroSchemaJSON);
+
 
 const kafkaClient = new Kafka({clientId: 'data-ingestion-kafka-producer', brokers: ['localhost:9092']})
 const kafkaConsumer = kafkaClient.consumer({ groupId: 'ingestion' })
@@ -9,7 +15,7 @@ await kafkaConsumer.subscribe({ topic: 'location', fromBeginning: true })
 await kafkaConsumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
-        value: message!.value!.toString(),
+        value: locationAvroType.fromBuffer(message!.value!)
       })
     },
   })
--- Kafka/kafkaProducer.ts
+++ Kafka/kafkaProducer.ts
@@ -1,20 +1,26 @@
 import { sleep } from 'bun'
 import { Kafka } from 'kafkajs'
+import { faker } from '@faker-js/faker'
+import avro from 'avsc';
+
+const locationAvroSchemaFile = Bun.file('./locationAvroSchema.json')
+const locationAvroSchemaJSON = await locationAvroSchemaFile.json()
+const locationAvroType = avro.Type.forSchema(locationAvroSchemaJSON);
 
 const kafkaClient = new Kafka({clientId: 'data-ingestion-kafka-producer', brokers: ['localhost:9092']})
 const kafkaProducer = kafkaClient.producer()
 await kafkaProducer.connect()
 
-let counter = 0
 while(true) {
-
+    let coordinatesArray = faker.location.nearbyGPSCoordinate()
+    let coordinatesBuffer = locationAvroType.toBuffer({latitude: coordinatesArray[0], longitude: coordinatesArray[1], timestamp: Date.now()})
+    
     await kafkaProducer.send({
         topic: 'location',
         messages: [
-            {value: counter.toString()}
+            {value: coordinatesBuffer}
         ]
     })
 
-    counter ++;
-    await sleep(1000);
+    await sleep(5000);
 }
